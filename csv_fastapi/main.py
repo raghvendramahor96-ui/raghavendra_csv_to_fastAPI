2d2a3f8265b7cc6d909c55b95b1ae7e7253221f7
abc <abc@gmail.com>
2026-03-26 00:51:51 +0530

    Added new FastAPI main file

--- csv_fastapi/main.py
+++ csv_fastapi/main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import pandas as pd
 
 app = FastAPI()
 
 df = pd.read_csv("students_complete.csv")
 
-@app.get("/")
+# Serve UI
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return {"message": "Student API is running 🚀"}
+    with open("index.html", "r", encoding="utf-8") as f:
+        return f.read()
 
+# API - all students
 @app.get("/students")
 def get_all_students():
     return df.fillna("").to_dict(orient="records")
 
+# API - limited students
 @app.get("/students/{limit}")
 def get_limited_students(limit: int):
     return df.head(limit).fillna("").to_dict(orient="records")
